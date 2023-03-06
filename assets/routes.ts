bd85a273c9f69bf4848aaebb68560f5fb6e099c3
afilipovski <filipovskia@yahoo.com>
2023-03-06 20:18:05 +0100

    Auto-generated commit [ci skip]

---
+++ assets/routes.ts
@@ -0,0 +1,6 @@
+export let gradovi: { name: string; lat: number; lon: number }[] = [
+  { name: 'Skopje', lat: 41.9981, lon: 21.4254 },
+  { name: 'Bitola', lat: 41.0297, lon: 21.3292 },
+  { name: 'Ohrid', lat: 41.1231, lon: 20.8016 },
+  { name: 'Mavrovo', lat: 41.6549, lon: 20.7339 },
+];
