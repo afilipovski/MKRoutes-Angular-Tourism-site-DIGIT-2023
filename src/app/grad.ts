6a4c5e187dd76af63cc67e687a04cc30bbab643c
Aleksandar <filipovskia@yahoo.com>
2023-03-08 14:03:19 +0100

    merge

--- src/app/grad.ts
+++ src/app/grad.ts
@@ -1,7 +1,12 @@
 export interface IGrad { 
     name: string;
-    lat: number;
-    lon: number; 
+    lat ?: number;
+    lon ?: number; 
     img: string;
     desc: string;
+    // cities
+    sights?: string[];
+    accommodation?: string[];
+    // hotels
+    link ?: string;
 }
