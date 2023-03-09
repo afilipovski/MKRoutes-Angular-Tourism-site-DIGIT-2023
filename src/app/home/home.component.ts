2f84e9e98b77f1fa3f50ffb1469e4bae9e0e811d
DimitrijKrstev <antishadowss@gmail.com>
2023-03-09 13:33:01 +0100

    navbar scroll buttons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { gradovi } from 'src/assets/routes';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  gradovi2 = gradovi.filter((grad) => grad.type === 'city');
+  constructor(private router: Router) {}
 
+  randomLocation() {
+    this.router.navigate([
+      `/place/${this.gradovi2
+        .at(Math.floor(Math.random() * this.gradovi2.length))
+        ?.name.toLowerCase()}`,
+    ]);
+  }
 }
