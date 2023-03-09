264a8530426e2d6ce114a948be631d1445b592ab
Aleksandar <filipovskia@yahoo.com>
2023-03-09 14:44:10 +0100

    bubbling

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,31 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
+  host: {
+    '(document:click)': 'onDocumentClick($event)',
+  },
 })
 export class NavbarComponent {
+  @ViewChildren('signIn') signInComponent : any;
+
   signInVisible: boolean = false;
+
+
+
+  onDocumentClick(event : any) {
+    this.signInVisible = false;  
+  }
+  onDivClick(event : any) {
+    event.stopPropagation();
+  }
+
+  toggle(event : any) {
+    this.signInVisible = !this.signInVisible;
+    event.stopPropagation();
+  }
 
   scroll(id: string) {
     let el = document.getElementById(id);
